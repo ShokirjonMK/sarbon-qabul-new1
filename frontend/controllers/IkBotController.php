b925e2f0b1d462ac34bf40c2c8fb47db64605fe0
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-13 14:00:27 +0500

    Update IkBotController.php

--- frontend/controllers/IkBotController.php
+++ frontend/controllers/IkBotController.php
@@ -28,46 +28,44 @@ class IkBotController extends Controller
         $username = $telegram->input->message->chat->username;
         $telegram_id = $telegram->input->message->chat->id;
 
-        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        $telegram = Yii::$app->telegram;
-        $telegram_id = $telegram->input->message->chat->id;
+        $message = "Sarbon universitetida qabul boshlandi<br><br>";
+        $message .= "2025-2026-o‘quv yillari uchun <b>kunduzgi, kechki va masofaviy ta'lim shakllariga</b> Sarbon universitetida qabul boshlanganini e'lon qilamiz.<br><br>";
+        $message .= "Biz bilan yetakchilar safida bo'!<br><br>";
+        $message .= "<b>Mavjud yo‘nalishlarimiz:</b><br>";
 
-// Xabar matni
-        $message = "Sarbon universitetida qabul boshlandi\n\n";
-        $message .= "2025\-2026\-o‘quv yillari uchun *kunduzgi, kechki va masofaviy ta'lim shakllariga* Sarbon universitetida qabul boshlanganini e'lon qilamiz.\n\n";
-        $message .= "Biz bilan yetakchilar safida bo'\!\n\n";
-        $message .= "*Mavjud yo‘nalishlarimiz:*\n";
-        $message .= "> Yurisdprudentsiya\n";
-        $message .= "> Davlat va jamiyat boshqaruvi\n";
-        $message .= "> Kosmetologiya\n";
-        $message .= "> Buxgalteriya hisobi\n";
-        $message .= "> Iqtisodiyot\n";
-        $message .= "> Bank ishi\n";
-        $message .= "> Moliya va moliyaviy texnologiyalar\n";
-        $message .= "> Jahon iqtisodiyoti va xalqaro iqtisodiy munosabatlar\n";
-        $message .= "> Biznesni boshqarish\n";
-        $message .= "> Logistika\n";
-        $message .= "> Marketing\n";
-        $message .= "> Xalqaro munosabatlar\n";
-        $message .= "> Turizm va mehmondo‘stlik\n";
-        $message .= "> Psixologiya\n";
-        $message .= "> Tarix\n";
-        $message .= "> Milliy g‘oya\, ma’naviyat asoslari va huquq ta’limi\n";
-        $message .= "> Filologiya va tillarni o‘qitish \(ingliz tili\, rus tili\, xitoy tili\, o‘zbek tili\, turk tili\)\n";
-        $message .= "> Axborot tizimlari va texnologiyalari\n";
-        $message .= "> Axborot xavfsizligi\n";
-        $message .= "> Kompyuter injiniringi\n";
-        $message .= "> Mexatronika va robototexnika\n";
-        $message .= "> Dizayn\n";
-        $message .= "> Arxitektura\n";
-        $message .= "> Qurilish muhandisligi \(Neft va gazni qayta ishlash sanoati obyektlari faoliyati turi bo‘yicha\)\n";
-        $message .= "> Kommunal infratuzilmani tashkil etish va boshqarish";
+        $message .= "<blockquote>";
+        $message .= "• Yurisdprudentsiya<br>";
+        $message .= "• Davlat va jamiyat boshqaruvi<br>";
+        $message .= "• Kosmetologiya<br>";
+        $message .= "• Buxgalteriya hisobi<br>";
+        $message .= "• Iqtisodiyot<br>";
+        $message .= "• Bank ishi<br>";
+        $message .= "• Moliya va moliyaviy texnologiyalar<br>";
+        $message .= "• Jahon iqtisodiyoti va xalqaro iqtisodiy munosabatlar<br>";
+        $message .= "• Biznesni boshqarish<br>";
+        $message .= "• Logistika<br>";
+        $message .= "• Marketing<br>";
+        $message .= "• Xalqaro munosabatlar<br>";
+        $message .= "• Turizm va mehmondo‘stlik<br>";
+        $message .= "• Psixologiya<br>";
+        $message .= "• Tarix<br>";
+        $message .= "• Milliy g‘oya, ma’naviyat asoslari va huquq ta’limi<br>";
+        $message .= "• Filologiya va tillarni o‘qitish (ingliz tili, rus tili, xitoy tili, o‘zbek tili, turk tili)<br>";
+        $message .= "• Axborot tizimlari va texnologiyalari<br>";
+        $message .= "• Axborot xavfsizligi<br>";
+        $message .= "• Kompyuter injiniringi<br>";
+        $message .= "• Mexatronika va robototexnika<br>";
+        $message .= "• Dizayn<br>";
+        $message .= "• Arxitektura<br>";
+        $message .= "• Qurilish muhandisligi (Neft va gazni qayta ishlash sanoati obyektlari faoliyati turi bo‘yicha)<br>";
+        $message .= "• Kommunal infratuzilmani tashkil etish va boshqarish<br>";
+        $message .= "</blockquote>";
 
-// Yuborish
+// Xabar yuborish
         return $telegram->sendMessage([
             'chat_id' => $telegram_id,
             'text' => $message,
-            'parse_mode' => 'MarkdownV2'
+            'parse_mode' => 'HTML'
         ]);
 
 
