b7fff4b95bde96579dea44ef194dfbc8f6dd6b31
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-13 13:59:04 +0500

    Update IkBotController.php

--- frontend/controllers/IkBotController.php
+++ frontend/controllers/IkBotController.php
@@ -28,10 +28,14 @@ class IkBotController extends Controller
         $username = $telegram->input->message->chat->username;
         $telegram_id = $telegram->input->message->chat->id;
 
-        // Xabar matni
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $telegram = Yii::$app->telegram;
+        $telegram_id = $telegram->input->message->chat->id;
+
+// Xabar matni
         $message = "Sarbon universitetida qabul boshlandi\n\n";
-        $message .= "2025-2026-o‘quv yillari uchun *kunduzgi, kechki va masofaviy ta'lim shakllariga* Sarbon universitetida qabul boshlanganini e'lon qilamiz.\n\n";
-        $message .= "Biz bilan yetakchilar safida bo'!\n\n";
+        $message .= "2025\-2026\-o‘quv yillari uchun *kunduzgi, kechki va masofaviy ta'lim shakllariga* Sarbon universitetida qabul boshlanganini e'lon qilamiz.\n\n";
+        $message .= "Biz bilan yetakchilar safida bo'\!\n\n";
         $message .= "*Mavjud yo‘nalishlarimiz:*\n";
         $message .= "> Yurisdprudentsiya\n";
         $message .= "> Davlat va jamiyat boshqaruvi\n";
@@ -48,23 +52,24 @@ class IkBotController extends Controller
         $message .= "> Turizm va mehmondo‘stlik\n";
         $message .= "> Psixologiya\n";
         $message .= "> Tarix\n";
-        $message .= "> Milliy g‘oya, ma’naviyat asoslari va huquq ta’limi\n";
-        $message .= "> Filologiya va tillarni o‘qitish (ingliz tili, rus tili, xitoy tili, o‘zbek tili, turk tili)\n";
+        $message .= "> Milliy g‘oya\, ma’naviyat asoslari va huquq ta’limi\n";
+        $message .= "> Filologiya va tillarni o‘qitish \(ingliz tili\, rus tili\, xitoy tili\, o‘zbek tili\, turk tili\)\n";
         $message .= "> Axborot tizimlari va texnologiyalari\n";
         $message .= "> Axborot xavfsizligi\n";
         $message .= "> Kompyuter injiniringi\n";
         $message .= "> Mexatronika va robototexnika\n";
         $message .= "> Dizayn\n";
         $message .= "> Arxitektura\n";
-        $message .= "> Qurilish muhandisligi\n";
+        $message .= "> Qurilish muhandisligi \(Neft va gazni qayta ishlash sanoati obyektlari faoliyati turi bo‘yicha\)\n";
         $message .= "> Kommunal infratuzilmani tashkil etish va boshqarish";
 
-// Xabar yuborish
+// Yuborish
         return $telegram->sendMessage([
             'chat_id' => $telegram_id,
             'text' => $message,
             'parse_mode' => 'MarkdownV2'
         ]);
+
 
 //        $photoUrl = "https://qabul.sarbon.university/frontend/web/images/new_bino.jpg";
 //        return $telegram->sendPhoto([
