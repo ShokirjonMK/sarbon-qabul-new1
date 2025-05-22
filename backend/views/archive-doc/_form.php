ffbda867296747ace03de05bb5a8e472cdbf038a
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-22 19:24:10 +0500

    Update _form.php

--- backend/views/archive-doc/_form.php
+++ backend/views/archive-doc/_form.php
@@ -1,6 +1,11 @@
 <?php
 
+use common\models\Exam;
 use common\models\Student;
+use common\models\StudentDtm;
+use common\models\StudentMaster;
+use common\models\StudentPerevot;
+use common\models\User;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
@@ -10,6 +15,28 @@ use kartik\select2\Select2;
 /** @var yii\web\View $this */
 /** @var app\models\ArchiveDoc $model */
 /** @var yii\widgets\ActiveForm $form */
+
+$query = Student::find()
+    ->alias('s')
+    ->innerJoin(User::tableName() . ' u', 's.user_id = u.id')
+    ->leftJoin(Exam::tableName() . ' e', 's.id = e.student_id AND e.status = 3 AND e.is_deleted = 0')
+    ->leftJoin(StudentPerevot::tableName() . ' sp', 's.id = sp.student_id AND sp.file_status = 2 AND sp.is_deleted = 0')
+    ->leftJoin(StudentDtm::tableName() . ' sd', 's.id = sd.student_id AND sd.file_status = 2 AND sd.is_deleted = 0')
+    ->leftJoin(StudentMaster::tableName() . ' sm', 's.id = sm.student_id AND sm.file_status = 2 AND sm.is_deleted = 0')
+    ->where([
+        'u.step' => 5,
+        'u.status' => [9, 10],
+        'u.user_role' => 'student',
+        's.is_deleted' => 0,
+    ])
+    ->andWhere(getConsIk())
+    ->andWhere([
+        'or',
+        ['not', ['e.student_id' => null]],
+        ['not', ['sp.student_id' => null]],
+        ['not', ['sd.student_id' => null]],
+        ['not', ['sm.student_id' => null]]
+    ])->all();
 ?>
 
 <div class="archive-doc-form">
@@ -18,11 +45,7 @@ use kartik\select2\Select2;
 
     <?= $form->field($model, 'student_id')->widget(Select2::class, [
         'data' => ArrayHelper::map(
-            Student::find()
-                ->joinWith('user')
-                ->where(['student.is_deleted' => 0])
-                ->andWhere(['user.status' => [9, 10]])
-                ->all(),
+            $query,
             'id',
             function ($model) {
                 return $model->first_name . ' ' . $model->last_name . ' ' . $model->middle_name .
