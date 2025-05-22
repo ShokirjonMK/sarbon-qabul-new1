38b81fbb093caf41dd822a5de59481fbc05d4bf9
ShokirjonMK <mkshokirjon@gmail.com>
2025-05-22 18:18:22 +0500

    Update create.php

--- backend/views/archive-doc/create.php
+++ backend/views/archive-doc/create.php
@@ -5,11 +5,30 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\ArchiveDoc $model */
 
-$this->title = 'Create Archive Document';
-$this->params['breadcrumbs'][] = ['label' => 'Archive Documents', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Qo‘shish';
+$breadcrumbs = [];
+$breadcrumbs['item'][] = [
+    'label' => Yii::t('app', 'Bosh sahifa'),
+    'url' => ['/'],
+];
+$breadcrumbs['item'][] = [
+    'label' => Yii::t('app', 'Arxiv'),
+    'url' => ['index'],
+];
+\yii\web\YiiAsset::register($this);
 ?>
 <div class="archive-doc-create">
+    <nav aria-label="breadcrumb" class="mb-4">
+        <ol class="breadcrumb">
+            <?php foreach ($breadcrumbs['item'] as $item) : ?>
+                <li class='breadcrumb-item'>
+                    <?= Html::a($item['label'], $item['url'], ['class' => '']) ?>
+                </li>
+            <?php endforeach; ?>
+            <li class="breadcrumb-item active" aria-current="page"><?= Html::encode($this->title) ?></li>
+        </ol>
+    </nav>
+
 
     <h1><?= Html::encode($this->title) ?></h1>
 
