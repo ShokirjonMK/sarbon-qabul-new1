f75e9dd00c49730f9be08b87ff1c04d423f028be
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-06-17 09:47:03 +0500

    Update MenuController.php

--- backend/controllers/MenuController.php
+++ backend/controllers/MenuController.php
@@ -27,41 +27,6 @@ class MenuController extends Controller
 
     public function actionIndex()
     {
-        $transaction = \Yii::$app->db->beginTransaction();
-
-        $startTime = strtotime('2025-06-14 00:00:00');
-        $endTime = strtotime('2025-06-14 23:59:59');
-
-        $exams = Exam::find()
-            ->where(['is_deleted' => 0])
-            ->andWhere(['between', 'start_time', $startTime, $endTime])
-            ->andWhere(['>', 'status', 1])
-            ->andWhere(['in', 'student_id',
-                Student::find()
-                    ->select('id')
-                    ->where(['exam_type' => 1])
-            ])
-            ->all();
-
-        foreach ($exams as $exam) {
-            $questions = ExamStudentQuestions::find()
-                ->where([
-                    'exam_id' => $exam->id,
-                ])
-                ->all();
-            foreach ($questions as $question) {
-                if ($question->option_id != null) {
-                    $option = Options::findOne($question->option_id);
-                    $question->is_correct = $option ? $option->is_correct : 0;
-                    $question->save(false);
-                }
-            }
-
-            Test::finishExam($exam);
-        }
-
-        $transaction->commit();
-        dd(2323232);
         $searchModel = new MenuSearch();
         $dataProvider = $searchModel->search($this->request->queryParams);
 
